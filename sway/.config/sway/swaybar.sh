57feed9e9760d00960565f194c22f957681720a6
Henrique Lecco <henriquealmeidalecco@gmail.com>
2021-11-02 17:24:53 -0300

    Made sway configuration more generic

--- sway/.config/sway/swaybar.sh
+++ sway/.config/sway/swaybar.sh
@@ -4,8 +4,7 @@ then
 	BLINK=':'
 fi
 
-MOUSEPROFILE=$(cat ~/.config/ratbag/current_profile)
 DATE=$(date +"%a %d/%m %H$BLINK%M")
 BAT=$(acpi -b | grep "Battery 0" | awk '{gsub(/%,/,""); print $4}')
 
-echo $(cat ~/.config/sway/message) Profile: $MOUSEPROFILE \| $DATE \| $BAT
+echo $DATE \| $BAT
